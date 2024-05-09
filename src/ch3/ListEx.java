759e53d7200f4de0fa53ba6ee7424151ef0e8716
DongWan <dlehddhks125@naver.com>
2024-05-09 10:00:09 +0900

    ListEx 객체 생성

--- src/ch3/ListEx.java
+++ src/ch3/ListEx.java
@@ -1,8 +1,11 @@
 package ch3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListEx {
 
     public static void main(String[] args) {
-
+        List<Integer> list = new ArrayList<>();
     }
 }
